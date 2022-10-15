11014c473f5114168f061165e9afea5228d52a91
Lucas2 <contatolucasoficial0@gmail.com>
2022-10-15 19:08:44 -0300

    Commit 3 - Código com agrupamento (Batching)

--- core/src/com/libgdx/graficos/Graphics.java
+++ core/src/com/libgdx/graficos/Graphics.java
@@ -7,9 +7,8 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;  //biblioteca interna de libGDX que possui funções úteis de matemática
 
 /*
-* O código anterior usa o método shapeRenderer.begin() para instruir ao libGDX (e à OpenGL) sobre a categoria de desenho à ser
-* construído (no caso, foi um círculo preenchido [filled]) e usa o método shapeRenderer.end() para garantir que o desenho
-* produzido foi renderizado corretamente. Porém, para vários desenhos, a eficiência do código pode ser baixa:
+* Apesar do código anterior aparentar ser ineficiente, basta uma simples modificação para alterar as coisas:
+* Com o conceito de agrupamento (Batching), pode-se agrupar chamadas semelhantes juntas, para ter maior eficiência.
 */
 
 public class Graphics extends ApplicationAdapter {  //Main class (classe principal)
@@ -25,13 +24,15 @@ public class Graphics extends ApplicationAdapter {  //Main class (classe princip
 		Gdx.gl.glClearColor(.25f,.25f,.25f,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		for(int i = 0; i < 10; i++) {  //loop (10 vezes): para criar 10 círculos de cores e posições diferentes
-			shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		//Para agrupar de maneira mais organizada, basta uma alteração singela:
+
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		for(int i = 0; i < 10; i++) {  //o loop agora só altera os parâmetros do desenho, ao invés de recriá-los várias vezes
 			shapeRenderer.setColor(MathUtils.random(), MathUtils.random(), MathUtils.random(), 1);  //gera cor aleatória
 			//Gera largura e altura aleatórias:
 			shapeRenderer.circle(MathUtils.random(Gdx.graphics.getWidth()), MathUtils.random(Gdx.graphics.getHeight()), 70);
-			shapeRenderer.end();
 		}
+		shapeRenderer.end();  //todos os desenhos são renderizados de uma só vez, juntos
 
 		System.out.println(Gdx.graphics.getFramesPerSecond());  //Imprime a taxa de frames por segundo
 	}
