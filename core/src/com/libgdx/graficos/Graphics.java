eb6a17f4c6353c8c44fe5b03024c462b2358e720
Lucas2 <contatolucasoficial0@gmail.com>
2022-10-17 14:21:05 -0300

    Commit 7 - Zoom e Rotação

--- core/src/com/libgdx/graficos/Graphics.java
+++ core/src/com/libgdx/graficos/Graphics.java
@@ -5,71 +5,91 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.OrthographicCamera;    //Classe que facilita manipulações de câmera
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 /*
-* Em cenários simples, o código anterior pode ser útil, mas em situações complexas, é recomendável utilizar os recursos da
-* classe Camera e suas subclasses OrthographicCamera e PerspectiveCamera, que permitem uma manipulação de cena conforme os
-* desenhos. Este código é uma réplica do anterior, porém utilizando tais recursos:
+* A classe Camera também oferece funcionalidades mais avançadas de manipulação de tela, com zoom e rotação:
 */
 
 public class Graphics extends ApplicationAdapter {  //Main class (classe principal)
 	ShapeRenderer shapeRenderer;
-	OrthographicCamera camera;  //variável de referência para as manipulações com a subclasse
+	OrthographicCamera camera;
 
-	float infEsqX = 0, infEsqY = 0, largRet, altRet, velocidade = 100f, vel;  //variáveis
+    //variáveis
+	float infEsqX = 0, infEsqY = 0, largRet, altRet, velocidade = 100f, vel, zVel, rVel;
+    float zoomVel = 1f, rotacaoVel = 20f;
 
 	@Override
 	public void create(){
-		//Cria uma nova instância da subclasse com as dimensões da janela de exibição, especificadas:
 		camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());  //os parâmetros são as dimensões
-
-		//Define a posição da câmera como o centro da cena. Para calcular o ponto, poderiam ser usadas quaisquer coordenadas.
-		camera.position.set(Gdx.graphics.getWidth()/2f, Gdx.graphics.getHeight()/2f,0);  //eixos x, y e z (não há)
-
-		//Calcula tudo o que for necessário diante das especificações definidas (nesse caso, dimensões e posição):
+		camera.position.set(Gdx.graphics.getWidth()/2f, Gdx.graphics.getHeight()/2f,0);  //posição inicial
 		camera.update();
 
 		shapeRenderer = new ShapeRenderer();
-		largRet = Gdx.graphics.getWidth() / 2f;  //Definição da largura padrão dos retângulos
-		altRet = Gdx.graphics.getHeight() / 2f;  //Definição da altura padrão dos retângulos
+		largRet = Gdx.graphics.getWidth() / 2f;
+		altRet = Gdx.graphics.getHeight() / 2f;
 	}
 
 	@Override
 	public void render(){
-		vel = velocidade * Gdx.graphics.getDeltaTime();  //Definição da velocidade baseada no frame rate (p/ facilitar)
+		vel = velocidade * Gdx.graphics.getDeltaTime();
+        zVel = zoomVel * Gdx.graphics.getDeltaTime();
+        rVel = rotacaoVel * Gdx.graphics.getDeltaTime();
+        //OBS.: as atribuições acima devem ocorrer aqui para que seu efeito seja efetivo a cada frame
 
-		//O método translate pode mover a câmera de tela. No caso, será movida para cima, baixo, direita e esquerda.
-		//Porém, é possível utilizar outras referências para mover a câmera, com a posição de um jogador.
-
-		if(Gdx.input.isKeyPressed(Input.Keys.W) || Gdx.input.isKeyPressed(Input.Keys.UP)){  //tecla W ou CIMA
+        //Move a câmera para cima ou para baixo:
+		if(Gdx.input.isKeyPressed(Input.Keys.W) || Gdx.input.isKeyPressed(Input.Keys.UP)){
 			camera.translate(0,-vel);
 		}
-		else if(Gdx.input.isKeyPressed(Input.Keys.S) || Gdx.input.isKeyPressed(Input.Keys.DOWN)){  //tecla S ou BAIXO
+		else if(Gdx.input.isKeyPressed(Input.Keys.S) || Gdx.input.isKeyPressed(Input.Keys.DOWN)){
 			camera.translate(0,vel);
 		}
-		if(Gdx.input.isKeyPressed(Input.Keys.D) || Gdx.input.isKeyPressed(Input.Keys.RIGHT)){  //tecla D ou DIREITA
+
+        //Move a câmera para esquerda ou direita:
+		if(Gdx.input.isKeyPressed(Input.Keys.D) || Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
 			camera.translate(-vel,0);
 		}
-		else if(Gdx.input.isKeyPressed(Input.Keys.A) || Gdx.input.isKeyPressed(Input.Keys.LEFT)){  //tecla A ou ESQUERDA
+		else if(Gdx.input.isKeyPressed(Input.Keys.A) || Gdx.input.isKeyPressed(Input.Keys.LEFT)){
 			camera.translate(vel,0);
 		}
+
+        //Ampliação ou redução de câmera (zoom):
+        if(Gdx.input.isKeyPressed(Input.Keys.NUMPAD_SUBTRACT)){  // MENOS (-) do teclado numérico
+            camera.zoom -= zVel;  //afastamento  (-zoom)
+        }
+        else if(Gdx.input.isKeyPressed(Input.Keys.NUMPAD_ADD)){   // MAIS (+) do teclado numérico
+            camera.zoom += zVel;  //aproximação  (+zoom)
+        }
+
+        //Rotação de câmera:
+        if(Gdx.input.isKeyPressed(Input.Keys.J)){  //tecla J
+            camera.rotate(-rVel);   //sentido horário (clockwise)
+        }
+        else if(Gdx.input.isKeyPressed(Input.Keys.F)){  //tecla F
+            camera.rotate(rVel);  //sentido anti-horário (counterclockwise)
+        }
+
+        /*
+        OBS.:
+        O movimento de câmera é afetado pelos efeitos de zoom e rotação, isto é, ao girar a câmera, por exemplo, gira-se
+        também os eixos x, y e z, que são as referências da câmera, e com isso, ao mover os objetos para 'cima', eles
+        irão movimentar-se em direção ao topo do eixo y, e como este está deslocado, o movimento parecerá estranho.
+        */
 
 		Gdx.gl.glClearColor(.25f,.25f,.25f,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		camera.update();
-		//O comando a seguir define que shapeRenderer usará as definições de Camera para desenhar tudo
-		shapeRenderer.setProjectionMatrix(camera.combined);  //o argumento passado é uma Matrix4, que representa a câmera
+		shapeRenderer.setProjectionMatrix(camera.combined);  //o argumento passado é a câmera
 
-		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);  //desenhos preenchidos
-		shapeRenderer.setColor(0,0,1,1);  //azul
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.setColor(0,0,1,1);
 		shapeRenderer.rect(infEsqX, infEsqY, largRet, altRet);  //retângulo criado na posição inferior esquerda (0,0)
-		shapeRenderer.setColor(1,0,0,1);  //vermelho
+		shapeRenderer.setColor(1,0,0,1);
 		shapeRenderer.rect(infEsqX + largRet, infEsqY, largRet, altRet);  //criado na posição superior esquerda
-		shapeRenderer.setColor(0,1,0,1);  //verde
+		shapeRenderer.setColor(0,1,0,1);
 		shapeRenderer.rect(infEsqX + largRet, infEsqY + altRet, largRet, altRet);  //posição superior direita
-		shapeRenderer.setColor(1,1,0,1);  //amarelo
+		shapeRenderer.setColor(1,1,0,1);
 		shapeRenderer.rect(infEsqX, infEsqY + altRet, largRet, altRet);  //posição inferior direita
 		shapeRenderer.end();
 	}
