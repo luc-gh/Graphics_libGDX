93331763ceb6809f0b258d5c00248b49902e819d
Lucas2 <contatolucasoficial0@gmail.com>
2022-10-16 21:20:06 -0300

    Commit 6 - Subclasse OrthographicCamera

--- core/src/com/libgdx/graficos/Graphics.java
+++ core/src/com/libgdx/graficos/Graphics.java
@@ -2,23 +2,34 @@ package com.libgdx.graficos;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;  //Biblioteca necessária para receber a entrada de usuário
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.OrthographicCamera;    //Classe que facilita manipulações de câmera
 
 /*
-* O movimento de câmera (ou do plano de tela do jogo) pode ser feito conforme os desenhos criados na tela, fazendo estes se
-* moverem de maneira lógica. Suponha que desejamos um movimento de tela como o do mundo do Super Mario.
-* Para criar um movimento assim, basta manipular as posições dos desenhos alterando suas coordenadas:
+* Em cenários simples, o código anterior pode ser útil, mas em situações complexas, é recomendável utilizar os recursos da
+* classe Camera e suas subclasses OrthographicCamera e PerspectiveCamera, que permitem uma manipulação de cena conforme os
+* desenhos. Este código é uma réplica do anterior, porém utilizando tais recursos:
 */
 
 public class Graphics extends ApplicationAdapter {  //Main class (classe principal)
 	ShapeRenderer shapeRenderer;
+	OrthographicCamera camera;  //variável de referência para as manipulações com a subclasse
 
-	float infEsqX = 0, infEsqY = 0, largRet, altRet, velocidade = 100f;  //variáveis
+	float infEsqX = 0, infEsqY = 0, largRet, altRet, velocidade = 100f, vel;  //variáveis
 
 	@Override
 	public void create(){
+		//Cria uma nova instância da subclasse com as dimensões da janela de exibição, especificadas:
+		camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());  //os parâmetros são as dimensões
+
+		//Define a posição da câmera como o centro da cena. Para calcular o ponto, poderiam ser usadas quaisquer coordenadas.
+		camera.position.set(Gdx.graphics.getWidth()/2f, Gdx.graphics.getHeight()/2f,0);  //eixos x, y e z (não há)
+
+		//Calcula tudo o que for necessário diante das especificações definidas (nesse caso, dimensões e posição):
+		camera.update();
+
 		shapeRenderer = new ShapeRenderer();
 		largRet = Gdx.graphics.getWidth() / 2f;  //Definição da largura padrão dos retângulos
 		altRet = Gdx.graphics.getHeight() / 2f;  //Definição da altura padrão dos retângulos
@@ -26,27 +37,31 @@ public class Graphics extends ApplicationAdapter {  //Main class (classe princip
 
 	@Override
 	public void render(){
-		if(Gdx.input.isKeyPressed(Input.Keys.W)){  //tecla W
-			infEsqY += velocidade * Gdx.graphics.getDeltaTime();  //move os retângulos p/ cima (reduz as coordenadas de Y)
+		vel = velocidade * Gdx.graphics.getDeltaTime();  //Definição da velocidade baseada no frame rate (p/ facilitar)
+
+		//O método translate pode mover a câmera de tela. No caso, será movida para cima, baixo, direita e esquerda.
+		//Porém, é possível utilizar outras referências para mover a câmera, com a posição de um jogador.
+
+		if(Gdx.input.isKeyPressed(Input.Keys.W) || Gdx.input.isKeyPressed(Input.Keys.UP)){  //tecla W ou CIMA
+			camera.translate(0,-vel);
 		}
-		else if(Gdx.input.isKeyPressed(Input.Keys.S)){  //tecla S
-			infEsqY -= velocidade * Gdx.graphics.getDeltaTime();  //move os retângulos p/ baixo (aumenta as coord. de Y)
+		else if(Gdx.input.isKeyPressed(Input.Keys.S) || Gdx.input.isKeyPressed(Input.Keys.DOWN)){  //tecla S ou BAIXO
+			camera.translate(0,vel);
 		}
-		if(Gdx.input.isKeyPressed(Input.Keys.D)){  //tecla D
-			infEsqX += velocidade * Gdx.graphics.getDeltaTime();  //move os retângulos p/ direita (reduz as coord. de X)
+		if(Gdx.input.isKeyPressed(Input.Keys.D) || Gdx.input.isKeyPressed(Input.Keys.RIGHT)){  //tecla D ou DIREITA
+			camera.translate(-vel,0);
 		}
-		else if(Gdx.input.isKeyPressed(Input.Keys.A)){  //tecla A
-			infEsqX -= velocidade * Gdx.graphics.getDeltaTime();  //move os retângulos p/ esquerda (aumenta as coord. de X)
+		else if(Gdx.input.isKeyPressed(Input.Keys.A) || Gdx.input.isKeyPressed(Input.Keys.LEFT)){  //tecla A ou ESQUERDA
+			camera.translate(vel,0);
 		}
 
 		Gdx.gl.glClearColor(.25f,.25f,.25f,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		//Criação dos retângulos (as coordenadas são redefinidas a cada frame [porque a função render faz exatamente isso])
-		/*
-		* Define-se uma cor (setColor) e depois são definidos:
-		* coordenada X, coordenada Y, largura e altura
-		*/
+		camera.update();
+		//O comando a seguir define que shapeRenderer usará as definições de Camera para desenhar tudo
+		shapeRenderer.setProjectionMatrix(camera.combined);  //o argumento passado é uma Matrix4, que representa a câmera
+
 		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);  //desenhos preenchidos
 		shapeRenderer.setColor(0,0,1,1);  //azul
 		shapeRenderer.rect(infEsqX, infEsqY, largRet, altRet);  //retângulo criado na posição inferior esquerda (0,0)
